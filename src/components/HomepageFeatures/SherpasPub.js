b8633a2de7d8faba11c7e4807c16cdb0e49cb98d
Chrstn67 <chrstn.hmbrt67.com>
2024-08-02 15:08:14 +0200

    MAJ page d'accueil

--- src/components/HomepageFeatures/SherpasPub.js
+++ src/components/HomepageFeatures/SherpasPub.js
@@ -1,6 +1,46 @@
 import React from "react";
 import sherpas from "./sherpas.module.css";
 
+// Import SVGs
+import MountainSvg from "@site/static/img/undraw_docusaurus_mountain.svg";
+import TreeSvg from "@site/static/img/undraw_docusaurus_tree.svg";
+import ReactSvg from "@site/static/img/undraw_docusaurus_react.svg";
+
+const teachingMethods = [
+  {
+    title: "Méthodologie vérifiée",
+    Svg: MountainSvg,
+    description:
+      "Tu apprendras avec une méthodologie qui a fait ses preuves, dans une ambiance détendue et propice à l'apprentissage. L'objectif est de t'aider à progresser efficacement et en toute sérénité.",
+  },
+  {
+    title: "Cours en ligne",
+    Svg: TreeSvg,
+    description:
+      "Tu as la possibilité de suivre des cours en présentiel, mais aussi et surtout en ligne. Cette flexibilité te permet de choisir le format qui te convient le mieux.",
+  },
+  {
+    title: "Le client, c'est l'élève !",
+    Svg: ReactSvg,
+    description:
+      "Il est possible d'adapter les horaires et les formules d'apprentissage en fonction de tes besoins et de ton budget. Que tu préfères des sessions intensives ou des cours plus espacés, plusieurs options te sont proposées pour répondre à tes attentes et te permettre de progresser à ton rythme.",
+  },
+];
+
+function Method({ Svg, title, description }) {
+  return (
+    <div className={sherpas.method}>
+      <div className={sherpas.svgContainer}>
+        <Svg className={sherpas.featureSvg} role="img" />
+      </div>
+      <div className={sherpas.textContainer}>
+        <h4>{title}</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
 function SherpasPub() {
   return (
     <div className={sherpas.sherpasPub}>
@@ -10,6 +50,11 @@ function SherpasPub() {
       <p className={sherpas.description}>
         Je donne également des cours particuliers en :
       </p>
+      <div className={sherpas.methodsWrapper}>
+        {teachingMethods.map((method, index) => (
+          <Method key={index} {...method} />
+        ))}
+      </div>
       <div className={sherpas.tableWrapper}>
         <table className={sherpas.subjectTable}>
           <thead>
@@ -107,7 +152,6 @@ function SherpasPub() {
           </tbody>
         </table>
       </div>
-
       <a
         href="https://sherpas.com/"
         target="_blank"
